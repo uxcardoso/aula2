bd0ea112301461fe72fccfa4aedd2446cf5de394
Thiago Cardoso <thiago.elementos@gmail.com>
2024-06-18 14:23:56 -0300

    realizei os exemplos da aula 2

---
+++ index.js
@@ -0,0 +1,11 @@
+let valor1 = prompt("Informe um número")
+let valor2 = prompt("Informe outro número")
+
+/*let numero1 = parseFloat(valor1)
+let numero2= parseFloat(valor2) */
+
+soma = parseFloat(valor1) + parseFloat(valor2)
+
+console.log(soma)
+
+alert(`O resultado da soma dos números informados é ${soma}`)
